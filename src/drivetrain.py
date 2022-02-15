eef9dca33d1437138bb43181b6e1af51c8596c88
PorterMurphy <56894897+PorterMurphy@users.noreply.github.com>
2022-02-15 07:55:06 -0800

    Added deadzone support on drive func
    formatted all python files
    
    Added support for deadzone_threahold float in drive func

--- src/drivetrain.py
+++ src/drivetrain.py
@@ -1,25 +1,42 @@
 import wpilib
 import wpilib.drive
+import wpilib.interfaces
+
 
 class DriveTrain:
-    def __init__(self,leftFront: wpilib.PWMMotorController,leftRear: wpilib.PWMMotorController,rightFront: wpilib.PWMMotorController,rightRear: wpilib.PWMMotorController) -> None:
+    def __init__(self, leftFront: wpilib.interfaces.MotorController, leftRear: wpilib.interfaces.MotorController, rightFront: wpilib.interfaces.MotorController, rightRear: wpilib.interfaces.MotorController) -> None:
         self.leftFront = leftFront
         self.leftRear = leftRear
         self.rightFront = rightFront
         self.rightRear = rightRear
-    def drive(self,Joystick: wpilib.Joystick) -> None:
+
+    def drive(self, Joystick: wpilib.Joystick, deadzone_threashold: float = 0) -> None:
         print("THIS SHOULD BE REPLACED!")
+
     def rightInverted(self, isInverted: bool) -> None:
         self.rightFront.setInverted(isInverted)
         self.rightRear.setInverted(isInverted)
+
     def leftInverted(self, isInverted: bool) -> None:
         self.leftFront.setInverted(isInverted)
         self.leftRear.setInverted(isInverted)
-    
+
+
 class MecanumDrive(DriveTrain):
-    def __init__(self, leftFront: wpilib.PWMMotorController, leftRear: wpilib.PWMMotorController, rightFront: wpilib.PWMMotorController, rightRear: wpilib.PWMMotorController) -> None:
-        super().__init__(leftFront, leftRear, rightFront, rightRear) # run the parent's __init__ function
-        self.MecanumDrive = wpilib.drive.MecanumDrive(self.leftFront, self.leftRear, self.rightFront, self.rightRear) # create a mecanum drive object
-    def drive(self, Joystick: wpilib.Joystick) -> None: # drive using this drivetrain
-        self.MecanumDrive.drivePolar(Joystick.getMagnitude(), Joystick.getDirectionDegrees(), Joystick.getTwist()) # drive using joystick
-    
+    def __init__(self, leftFront: wpilib.interfaces.MotorController, leftRear: wpilib.interfaces.MotorController, rightFront: wpilib.interfaces.MotorController, rightRear: wpilib.interfaces.MotorController) -> None:
+        # run the parent's __init__ function
+        super().__init__(leftFront, leftRear, rightFront, rightRear)
+        self.MecanumDrive = wpilib.drive.MecanumDrive(
+            self.leftFront, self.leftRear, self.rightFront, self.rightRear)  # create a mecanum drive object
+
+    # drive using this drivetrain
+    def drive(self, Joystick: wpilib.Joystick, deadzone_threashold: float = 0) -> None:
+        mag = Joystick.getMagnitude()
+        angle = Joystick.getDirectionDegrees()
+        rotate = Joystick.getTwist()
+        if mag < deadzone_threashold:
+            mag = 0
+        if abs(rotate) < deadzone_threashold:  # absolute value b/c rotate goes from -1 to 1
+            rotate = 0
+        self.MecanumDrive.drivePolar(
+            mag, angle, rotate)  # drive using joystick
