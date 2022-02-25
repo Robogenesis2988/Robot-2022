e75f6334ba167fe68146a5c5a4211a22191d441f
PorterMurphy <56894897+PorterMurphy@users.noreply.github.com>
2022-02-25 11:59:32 -0500

    Cleaning

--- src/winch.py
+++ src/winch.py
@@ -10,20 +10,21 @@ class winch:
         self.timer = wpilib.Timer()
         self.time = 0 
 
-    def winchExtendAdjustable(self):
+    def winchExtend(self):
         self.leftWinch.set(.5)
         self.rightWinch.set(.5)
     
-    def winchRetractAdjustable(self):
+    def winchRetract(self):
         self.leftWinch.set(-.5)
         self.rightWinch.set(-.5)
-
-    def winchExtendTimed(self):
-        self.leftWinch.set(.5)
-        self.rightWinch.set(.5)
-        self.timer.hasElapsed(self.time)
+    def winchStop(self):
         self.leftWinch.set(0)
         self.rightWinch.set(0)
+
+    def winchExtendTimed(self):
+        self.winchExtend()
+        self.timer.hasElapsed(self.time)
+        self.winchRetract()
 
     def winchRetractTimed(self):
         self.leftWinch.set(.5)
